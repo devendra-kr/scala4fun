65eda4bd43b52ac0239e77b785d5c5a2fa3deee8
Devendra Kumar <devendrak@yupptv.com>
2019-02-20 12:31:32 +0530

    made generic zip function

--- Zipper.scala
+++ Zipper.scala
@@ -7,15 +7,12 @@
 ========================================== */
 object Zipper{
 	def main(args: Array[String]): Unit = {
-		val resZip = zip(List(1,2,3), List("a", "b", "c"))
-		println(resZip)
+		println(zip(List(1,2,3), List("a", "b", "c")))
+		println(zip(List('a', 'b', 'c'), List(1,2,3)))
 	}
-
-	def zip(list1: List[Int], list2: List[String]): List[(Int, String)] = {
-	  	(list1, list2) match {
+	def zip[A,B](ls1: List[A], ls2: List[B]): List[(A, B)] = (ls1, ls2) match {
   			case(Nil,_) => Nil
 	  		case(_, Nil) => Nil
 	  		case(head1 :: tail1, head2 :: tail2) => (head1,head2)::zip(tail1,tail2)
-  		}
 	}
 }
