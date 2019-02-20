fc9e537e8c3b1318c5c08b481dae783bd9991a34
Devendra Kumar <devendrak@yupptv.com>
2019-02-20 11:58:29 +0530

    pure function for merge
    pure function for merge

--- Merge.scala
+++ Merge.scala
@@ -7,18 +7,13 @@
 ========================================== */
 object Merge{
         def main(args: Array[String]): Unit = {
-                val res = merge(List(1,2,3), List(4, 5, 6))
-                println(res)
+        	println(merge(Range(0,10).toList,Range(10,20).toList))
         }
-	def merge(list1: List[Int], list2: List[Int]): List[Int] = {
-   	 (list1, list2) match {
-	      case (Nil, list2) => list2
-	      case (list1, Nil) => list1
-	      case (list1, list2) => { var list = list1
-	      list2.foreach(x => {list :+= x})
-        list
-      }
-    }
-  }
-	
+	def merge(list1: List[Int], list2: List[Int]): List[Int] = (list1, list2) match {
+	      case (Nil, Nil) => Nil
+	      case (Nil, _) => list2
+	      case (_, Nil) => list1
+	      case (_, x :: y) => merge(list1 ++ List(x),y)
+      	}
+  	}
 }
