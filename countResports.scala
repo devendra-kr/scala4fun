1be1f3582015b91d439d0876c011bba2496a334d
Devendra Kumar <devendrak@yupptv.com>
2020-01-24 20:54:18 +0530

    bug fix

--- countResports.scala
+++ countResports.scala
@@ -5,11 +5,11 @@
     def count(ls: List[(Int, Int)], res: Int): Int = ls match {
       case Nil          => res
       case x :: Nil     => res
-      case x :: y :: xs => if (y._1 - x._1 == 1) count(y::xs, res + y._2) else count(y::xs, res)
+      case x :: y :: xs => if (y._1 - x._1 == 1) count(y::xs, res + x._2) else count(y::xs, res)
     }
     count(sortedList, 0)
   }
   
  /*ls1 = List(1,1,2,3,5,6,6) o/p ===> 4
-  ls2 = List(0,1,1,2,3,5,6,6) o/p ====> 6
+  ls2 = List(0,1,1,2,3,5,6,6) o/p ====> 5
   */
